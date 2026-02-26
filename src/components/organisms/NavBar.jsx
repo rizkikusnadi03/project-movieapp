e8acad0309b65fdd130b0f03a899f64c402a4a5e
rizkikusnadi03 <rizkikusnadi03@gmail.com>
2026-02-26 21:27:48 +0700

    update fitur

--- src/components/organisms/NavBar.jsx
+++ src/components/organisms/NavBar.jsx
@@ -1,18 +1,24 @@
 import { Link } from 'react-router-dom';
+import ProfileNav from '../atoms/ProfileNav';
+import chillLogo from '../../assets/chill-logo.png';
+import '../../Beranda.css';
 
 const NavBar = () => {
     return (
-        <nav className="navbar">
-            <div className="navbar-brand">Chill Movie</div>
-            <div className="navbar-links">
-                <Link to="/home">Dashboard</Link>
-                <Link to="/profile">Profile</Link>
-                <button onClick={() => {
-                    localStorage.removeItem('isAuthenticated');
-                    window.location.href = '/';
-                }}>Logout</button>
-            </div>
-        </nav>
+        <header>
+            <nav className="navbar">
+                <div className="logo">
+                    <img src={chillLogo} alt="Chill Movie" style={{ width: '100%' }} />
+                </div>
+                <div className="links">
+                    <Link to="/home">Beranda</Link>
+                    <Link to="/series">Series</Link>
+                    <Link to="/movies">Film</Link>
+                    <Link to="/new">Daftar Saya</Link>
+                    <ProfileNav />
+                </div>
+            </nav>
+        </header>
     );
 };
 
